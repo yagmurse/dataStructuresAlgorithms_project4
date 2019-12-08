a4297739b67227c0ddff4e5b8029ffb1dfbd763a
yagmurse <yselek14@gmail.com>
2019-12-08 12:59:10 +0300

    2017400273

--- HelpStudents.cpp
+++ HelpStudents.cpp
@@ -9,6 +9,7 @@ Notes: Anything you want to say about your code that will be helpful in the grad
 #include "HelpStudents.h"
 #include <limits>
 #include <queue>
+#include <stack>
 using namespace std;
 const int INF = numeric_limits<int>::max();
 HelpStudents::HelpStudents(int  N, int  M, int K, vector < pair< pair <int,int> , int > > ways) {
@@ -31,53 +32,72 @@ HelpStudents::HelpStudents(int  N, int  M, int K, vector < pair< pair <int,int> 
 
 long long int HelpStudents::firstStudent() {
     // IMPLEMENT ME!
-    vector<int> costNode;
-
+    vector<long long int> costNode;
     vector<int> visited;
-
     for(int i=0;i<N;i++) {
         visited.push_back(0);
     }
-    priority_queue<pair<int,int>,vector<pair<int,int>>,greater<pair<int,int>>> pq;
+    priority_queue<pair<long long int,int>,vector<pair<long long int,int>>,greater<pair<long long int,int>>> pq;
     for(int i=0;i<N;i++) {
-       costNode.push_back(0);
+       costNode.push_back(i==0 ? 0 :INF);
     }
-    int c=costNode[0];
-
-    pq.push(make_pair(0,0));
+     pq.push(make_pair(0,0));
     while (!pq.empty()) {
-        pair<int,int> tmp=pq.top();
+        pair<long long int,int> tmp=pq.top();
         int cost=tmp.first;
         int vertex=tmp.second;
-        //vertex: tmp.second............. cost:temp.first
         pq.pop();
-        if(visited[vertex]==1){
-        continue;}
-
+        if(visited[vertex]==1)
+        continue;
         visited[vertex]=1;
         costNode[vertex] = cost;
             for (int i = 0; i < (int) vec[vertex].size(); i++) {
                 int v = vec[vertex][i].first;
-                if (visited[v] == 1) {
-                    continue; }
-
-                    int c = vec[vertex][i].second + cost;
+                if (visited[v] == 1)
+                    continue;
+                long long int c = vec[vertex][i].second + cost;
                     pq.push(make_pair(c, v));
-
-
-        }
-
-
+            }
     }
 
-    long long int m;
-    m=costNode[K-1];
-    return m;
+    return costNode[K-1];
 
 
     }
 long long int HelpStudents::secondStudent() {
     // IMPLEMENT ME!
+    vector<long long int> minEdge;
+    for(int i=0;i<N;i++) {
+        minEdge.push_back(0);
+    }
+    vector<int> visited;
+    for(int i=0;i<N;i++) {
+        visited.push_back(0);
+    }
+     priority_queue<pair<long long int,int>,vector<pair<long long int,int>>,greater<pair<long long int,int>>> pq;
+    pq.push(make_pair(0,0));
+    while(!pq.empty()) {
+        pair<long long int,int> tmp=pq.top();
+        int vertex=tmp.second;
+        long long int edge=tmp.first;
+        pq.pop();
+        if(visited[vertex]==1) {
+            continue; }
+        visited[vertex]=1;
+        minEdge[vertex]=max(minEdge[vertex],edge);
+        for (int i = 0; i < (int) vec[vertex].size(); i++) {
+            int v = vec[vertex][i].first;
+            if (visited[v] == 1)
+                continue;
+
+            long long  int c =vec[vertex][i].second;
+            pq.push(make_pair(max(c,edge), v));
+        }
+
+    }
+
+    return minEdge[K-1];
+
 
 }
 long long int HelpStudents::thirdStudent() {
